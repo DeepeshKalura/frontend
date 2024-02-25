0d61f053e22ad0a8027a6151e658edadaf1f0c59
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-25 18:25:28 +0530

    Refactor User model to use nullable properties

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,64 +1,21 @@
-import 'dart:convert';
-
 class User {
-  final String name;
-  final String id;
-  final String createdAt;
-  User({
-    required this.name,
-    required this.id,
-    required this.createdAt,
-  });
+  double? creationTime;
+  String? id;
+  String? name;
 
-  User copyWith({
-    String? name,
-    String? token,
-    String? pyqScore,
-    String? id,
-    String? createdAt,
-  }) {
-    return User(
-      name: name ?? this.name,
-      id: id ?? this.id,
-      createdAt: createdAt ?? this.createdAt,
-    );
+  User({this.creationTime, this.id, this.name});
+
+  User.fromJson(Map<String, dynamic> json) {
+    creationTime = json['_creationTime'];
+    id = json['_id'];
+    name = json['name'];
   }
 
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'name': name,
-      'id': id,
-      'createdAt': createdAt,
-    };
-  }
-
-  factory User.fromMap(Map<String, dynamic> map) {
-    return User(
-      name: map['name'] as String,
-      id: map['id'] as String,
-      createdAt: map['createdAt'] as String,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory User.fromJson(String source) =>
-      User.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() {
-    return 'User(name: $name, id: $id, createdAt: $createdAt)';
-  }
-
-  @override
-  bool operator ==(covariant User other) {
-    if (identical(this, other)) return true;
-
-    return other.name == name && other.id == id && other.createdAt == createdAt;
-  }
-
-  @override
-  int get hashCode {
-    return name.hashCode ^ id.hashCode ^ createdAt.hashCode;
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['_creationTime'] = creationTime;
+    data['_id'] = id;
+    data['name'] = name;
+    return data;
   }
 }
