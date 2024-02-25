6cf2b25257502107da8b038310a75a7d5e38829b
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-25 10:50:26 +0530

    Remove token and pyqScore properties from User class

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,14 +2,10 @@ import 'dart:convert';
 
 class User {
   final String name;
-  final String token;
-  final String pyqScore;
   final String id;
   final String createdAt;
   User({
     required this.name,
-    required this.token,
-    required this.pyqScore,
     required this.id,
     required this.createdAt,
   });
@@ -23,8 +19,6 @@ class User {
   }) {
     return User(
       name: name ?? this.name,
-      token: token ?? this.token,
-      pyqScore: pyqScore ?? this.pyqScore,
       id: id ?? this.id,
       createdAt: createdAt ?? this.createdAt,
     );
@@ -33,8 +27,6 @@ class User {
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'name': name,
-      'token': token,
-      'pyqScore': pyqScore,
       'id': id,
       'createdAt': createdAt,
     };
@@ -43,8 +35,6 @@ class User {
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
       name: map['name'] as String,
-      token: map['token'] as String,
-      pyqScore: map['pyqScore'] as String,
       id: map['id'] as String,
       createdAt: map['createdAt'] as String,
     );
@@ -57,26 +47,18 @@ class User {
 
   @override
   String toString() {
-    return 'User(name: $name, token: $token, pyqScore: $pyqScore, id: $id, createdAt: $createdAt)';
+    return 'User(name: $name, id: $id, createdAt: $createdAt)';
   }
 
   @override
   bool operator ==(covariant User other) {
     if (identical(this, other)) return true;
 
-    return other.name == name &&
-        other.token == token &&
-        other.pyqScore == pyqScore &&
-        other.id == id &&
-        other.createdAt == createdAt;
+    return other.name == name && other.id == id && other.createdAt == createdAt;
   }
 
   @override
   int get hashCode {
-    return name.hashCode ^
-        token.hashCode ^
-        pyqScore.hashCode ^
-        id.hashCode ^
-        createdAt.hashCode;
+    return name.hashCode ^ id.hashCode ^ createdAt.hashCode;
   }
 }
