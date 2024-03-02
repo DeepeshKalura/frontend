b2cd07526e26fde3707235240bbbf2660efde79e
DeepeshKalura <deepeshkalurs@gmail.com>
2024-03-02 20:57:36 +0530

    Add chat functionality to HomeScreen

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,27 +1,75 @@
+import 'dart:math';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 import '../model/user.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.user});
   final User user;
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<types.Message> _messages = [];
+  types.User _user = const types.User(
+    id: '0',
+    firstName: 'not been set',
+  );
+  @override
+  void initState() {
+    super.initState();
+    setuser();
+  }
+
+  setuser() {
+    _user = types.User(
+      id: widget.user.id ?? '-1',
+      firstName: widget.user.name ?? 'Unknown',
+    );
+  }
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  String randomString() {
+    final random = Random.secure();
+    final values = List<int>.generate(16, (i) => random.nextInt(255));
+    return base64UrlEncode(values);
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      drawer: const Drawer(
-        backgroundColor: Colors.amber,
-      ),
-      body: const Column(
-        children: [
-          Center(
-            child: Text('Home Screen'),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Home Screen'),
+        ),
+        drawer: const Drawer(
+          backgroundColor: Colors.amber,
+        ),
+        body: Chat(
+          messages: _messages,
+          onSendPressed: _handleSendPressed,
+          user: _user,
+        ));
   }
 }
